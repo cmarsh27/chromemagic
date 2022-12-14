31e5b094df962b1bb856e8f6b3d842313e071aca
Calum Marshall CS2019 <calum.marshall.2019@uni.strath.ac.uk>
2022-12-14 14:36:53 +0000

    Initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Chrome Magic</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I made a React app for a magic shop productivity extention.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
   );
