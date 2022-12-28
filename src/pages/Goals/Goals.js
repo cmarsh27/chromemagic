4adf0bd645ef4f925ea443acef0cb09df8364010
Calum Marshall CS2019 <calum.marshall.2019@uni.strath.ac.uk>
2022-12-28 18:07:19 +0000

    Started Goals

--- src/pages/Goals/Goals.js
+++ src/pages/Goals/Goals.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const goalData = [{title : "Brush your damn teeth.", expires : "24", completed : "false"}]
 
 const Goals = () => {
+  const [newGoal, setNewGoal] = useState({title : "", expires : "24", completed : "false"})
+  const limit = 3;
+  const addGoal = (e) => {
+    e.preventDefault();
+    if (goalData.length < limit){
+      goalData.push(newGoal)
+      setNewGoal({title : "", expires : "24", completed : "false"})
+    }
+  }
+
   return (
     <>
-        <header className="header">
-            <h1>Chrome Magic</h1>
-            <p>
-            Hi, I made a React app for a magic shop productivity extension.
-            </p>
-        </header>
         <section className='section'>
-            <h2>Your Goals!</h2>
+            <h1>Your Goals!</h1>
             <p>Let's make some goals, shall we?</p>
+
+            <div>
+              <h2>These are your goals..</h2>
+                {goalData.map((goal, index) => 
+                <div key = {index}>
+                <p>{goal.title}  -   {goal.completed}</p> 
+                </div>
+                )}
+            </div>
+
+            { (goalData.length < limit) &&
+            <div>
+              <form onSubmit={addGoal}>
+                <input name = "title" type="text" placeholder='Walk the dog.' onChange={(e) => setNewGoal({ ...newGoal, title: e.target.value})}/>
+                <button>Add Goal</button>
+              </form>
+
+
+            </div>
+            }
         </section>
   </>
   )
