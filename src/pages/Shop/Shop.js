4353411af6e21c23cc64fb85ca53687da7309b28
KRobet <Kirsty545@gmail.com>
2023-01-01 17:03:14 +0000

    New fonts and styling

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,18 +1,20 @@
-import React, {usestate} from 'react'
+import React, {useState} from 'react'
 
 const itemData = [
-                  {title : "Invisibility potion", quantity : "10", rarity : "common"},
-                  {title : "Flying potion", quantity : "10", rarity: "common"},
-                  {title : "Big cauldron", quantity : "8", rarity: "common"},
-                  {title : "Pointy wizard hat", quantity : "3", rarity: "uncommon"},
-                  {title : "Shiny dragon egg", quantity : "1", rarity: "rare"},
-                  {title : "Floppy fish", quantity : "5", rarity: "uncommon"},
-                  {title : "Love potion", quantity : "10", rarity: "common"},
-                  {title : "Broken wand", quantity : "6", rarity: "common"}
+                  {title : "Invisibility potion", quantity : "10", rarity : "common", description : "Drink to turn invisible"},
+                  {title : "Flying potion", quantity : "10", rarity: "common", description : "Make sure you land before it wears off"},
+                  {title : "Big cauldron", quantity : "8", rarity: "common", description : "Use to brew big potions"},
+                  {title : "Pointy wizard hat", quantity : "3", rarity: "uncommon", description : "Very important wizard gear"},
+                  {title : "Shiny dragon egg", quantity : "1", rarity: "rare", description : "Keep it near the fire"},
+                  {title : "Floppy fish", quantity : "5", rarity: "uncommon", description : "Flop flop flop flop"},
+                  {title : "Love potion", quantity : "10", rarity: "common", description : "The most dangerous potion of all"},
+                  {title : "Broken wand", quantity : "6", rarity: "common", description : "Perhaps you can fix it?"}
                   ]
+                  
+let inventory=[]
 
 const Shop = () => {
-  
+
   const random = Math.floor(Math.random() * itemData.length)
   let randItems = []
   
@@ -35,12 +37,10 @@ const Shop = () => {
     <>
         <header className="header">
             <h1>What's for sale?</h1>
-            <p>
-            <b>You can buy items here!</b>
-            </p>
             {randItems.map((item, index) => 
-                <div key = {index} className={item.rarity}>
-                <p>{item.title} - {item.quantity}</p> 
+                <div key = {index} className={item.rarity} >
+                <h3>{item.title} - {item.quantity}</h3>
+                <p className='description'> {item.description} </p>
                 </div>
             )}
         </header>
